4079eac4fdb3273951a2bcc8bf7fccdb14556ed7
veepee <66334129+vpontual@users.noreply.github.com>
2024-03-26 18:43:50 -0400

    added possible license badges

--- utils/generateMardown.js
+++ utils/generateMardown.js
@@ -1,6 +1,17 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  const badges = {
+    MIT: "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)",
+    "Apache 2.0":
+      "[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)",
+    "GPL 3.0":
+      "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)",
+    "BSD 3-Clause":
+      "[![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)",
+    None: "",
+  };
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
