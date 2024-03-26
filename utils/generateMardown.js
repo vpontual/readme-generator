b243d97430aca1b8e844295dc81e7938e3d2fb08
veepee <66334129+vpontual@users.noreply.github.com>
2024-03-26 18:47:12 -0400

    added links for each license

--- utils/generateMardown.js
+++ utils/generateMardown.js
@@ -15,7 +15,17 @@ function renderLicenseBadge(license) {
 }
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  const licenseLinks = {
+    MIT: "https://opensource.org/licenses/MIT",
+    "Apache 2.0": "https://opensource.org/licenses/Apache-2.0",
+    "GPL 3.0": "https://www.gnu.org/licenses/gpl-3.0",
+    "BSD 3-Clause": "https://opensource.org/licenses/BSD-3-Clause",
+    None: "",
+  };
+
+  return licenseLinks[license];
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
