43c58c6395b7d86157de077ef1a30fd2b2008703
veepee <66334129+vpontual@users.noreply.github.com>
2024-03-26 19:07:11 -0400

    added function to write README file

--- index.js
+++ index.js
@@ -61,7 +61,11 @@ const questions = [
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+  fs.writeFile(fileName, data, (err) =>
+    err ? console.error(err) : console.log("README generated successfully!")
+  );
+}
 
 // TODO: Create a function to initialize app
 function init() {}
