d86f4df6d445ee03d70d1674d35bab25bb8f6483
veepee <66334129+vpontual@users.noreply.github.com>
2024-03-26 19:07:55 -0400

    completed function to initialize app

--- index.js
+++ index.js
@@ -68,7 +68,13 @@ function writeToFile(fileName, data) {
 }
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions).then((answers) => {
+        const markdown = generateMarkdown(answers);
+        writeToFile('README.md', markdown);
+      });
+    }
+}
 
 // Function call to initialize app
 init();
